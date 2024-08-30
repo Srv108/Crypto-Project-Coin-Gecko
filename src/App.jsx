4a6834a61173a772f119d3e4366b1ed36fa34a5e
Srv108 <srv.br009@gmail.com>
2024-08-30 16:49:50 +0530

    Navbar and Banner added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Component/Navbar/Navbar'
+import Banner from './Component/Banner/Banner'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Welcome to the HangMan</h1>
-      Welcome
+      <Navbar/>
+      <Banner/>
     </>
   )
 }
