4a83d4746a50fd43cdcfe4eda9a9dcde23560849
Srv108 <srv.br009@gmail.com>
2024-09-05 05:39:49 +0530

    Coins data embedded

--- src/Component/Page/Coins/CoinsDetailPage.jsx
+++ src/Component/Page/Coins/CoinsDetailPage.jsx
@@ -1,22 +1,71 @@
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import { fetchCoinDetails } from "../../../services/fetchCoinDetails";
+import { useEffect } from "react";
+import parse from 'html-react-parser';
 
 function CoinsDetailPage(){
 
     const {coinId} = useParams();
 
-    const {data} = useQuery(["coins",coinId] , () => fetchCoinDetails(coinId),{
-        retry : 2,
+    const {data: coin,isLoading,isError} = useQuery(["coins",coinId] , () => fetchCoinDetails(coinId),{
         cacheTime : 1000*60*2,
         staleTime : 1000*60*2,
     });
 
-    console.log(data);
+    useEffect(() => {
+        console.log(coin);
+    },[coin]);
+
+    if(isLoading){
+        return(
+            <div>Loading......</div>
+        );
+    }
+    if(isError){
+        return(
+            <div>Something went wrong....</div>
+        );
+    }
+
     return(
-        <div> 
-            <h1> Coins Details by their {coinId} </h1> 
-            
+        <div className="flex flex-col md:flex-row"> 
+            <div className="md:w-1/3 w-full flex flex-col items-center mt-6 md:mt-0 border-r-2 border-gray-500">
+                <img 
+                    className="h-52 mb-5"
+                    src={coin?.image?.large} 
+                    alt={coin?.name}
+                />
+                <h1
+                    className="text-4xl font-bold mb-5"
+                >   #
+                    <span className="ml-3 text-4xl ">
+                        {coin?.market_cap_rank}
+                    </span> 
+                </h1>
+                <h2
+                    className="text-4xl font-bold mb-5"
+                > 
+                    {coin?.name} 
+                </h2>
+                <div className="flex items-center mb-4 md:mb-0">
+                    <h2 className="text-xl text-yellow-400 font-bold ">
+                        Current Price
+                    </h2>
+                    <span className="ml-3 text-xl ">
+                        {coin?.market_data.current_price["usd"]}
+                    </span>
+                </div>
+
+                <p
+                    className="w-full px-6 py-4 text-justify" 
+                >
+                    {parse(coin?.description?.en)}
+                </p>
+            </div> 
+            <div className="md:w-2/3 w-full">
+                <h1>Coins Data</h1>
+            </div>
         </div>
     );
 }
--- src/services/fetchCoinDetails.js
+++ src/services/fetchCoinDetails.js
@@ -3,7 +3,7 @@ import axiosInstance from "../helpers/axiosInstance";
 export async function fetchCoinDetails(id){
     try{
         const response = await axiosInstance.get(`/coins/${id}`);
-        console.log(response.data);
+        console.log("Coins Data is ",response.data);
         return response.data;
     }catch(error){
         console.log(error);
